43c48fa1d6fab67d95da98faeb3afe8699d4122c
Nick Triantafyllidis <ntrianta@lab.grid.auth.gr>
2014-11-18 00:00:38 +0100

    more changes

--- main.go
+++ main.go
@@ -1,45 +1,52 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "math"
-  "io/ioutil"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 type Answer struct {
-  id		string
-  result    []byte
-  err      	error
+	short string
+	long  string
+	err   error
 }
 
-func myfunc() []*Answer{
-	var ab = []string{"a","b","c","d","e",}
+type Long struct {
+	LongUrl string `json:longUrl`
+}
+
+func myfunc() []*Answer {
+	var ab = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 	key := ""
 	addr := "https://www.googleapis.com/urlshortener/v1/url?shortUrl=http://goo.gl/"
-	
-    ch := make(chan *Answer)
-    results := []*Answer{}
-	for _, a := range ab{
-		for _, b := range ab{
-			for _, c := range ab{
-				for _, d := range ab{
-					id := fmt.Sprintf("%s%s%s%s",a,b,c,d)
-					url := fmt.Sprintf("%s%s&key=%s",addr,id,key)
-					go func (url string, id string){
-						resp, err:= http.Get(url)
-						result, _ := ioutil.ReadAll(resp.Body)
-					    resp.Body.Close()
-						ch <- &Answer{id, result, err}
-					}(url,id)	
+
+	ch := make(chan *Answer)
+	results := []*Answer{}
+
+	for _, c := range ab {
+		for _, d := range ab {
+			id := fmt.Sprintf("aa%s%s", c, d)
+			url := fmt.Sprintf("%s%s&key=%s", addr, id, key)
+			go func(url string, id string) {
+				l := Long{}
+				resp, err := http.Get(url)
+				result, _ := ioutil.ReadAll(resp.Body)
+				resp.Body.Close()
+				e := json.Unmarshal(result, &l)
+				if e != nil {
+					ch <- &Answer{"problem", "problem", e}
+				} else {
+					ch <- &Answer{id, l.LongUrl, err}
 				}
-			}
+			}(url, id)
 		}
 	}
 	for {
-	    r := <-ch
-	    results = append(results, r)
-	    if len(results) == int(math.Pow(5,4)) {
+		r := <-ch
+		results = append(results, r)
+		if len(results) == 3844 {
 			close(ch)
 			return results
 		}
@@ -49,12 +56,11 @@ func myfunc() []*Answer{
 
 func main() {
 	results := myfunc()
-	for _, r := range results{
-		if r.err != nil{
+	for _, r := range results {
+		if r.err != nil {
 			fmt.Println(r.err)
-		}else{
-			fmt.Println(r.id)
-			fmt.Printf("%s\n",r.result)
+		} else {
+			fmt.Println(r.long)
 		}
 	}
 }
