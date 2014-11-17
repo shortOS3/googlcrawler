7f8218c122e535f0251e9507ddfa4cc3c37bea77
Nick Triantafyllidis <ntrianta@lab.grid.auth.gr>
2014-11-17 21:21:59 +0100

    major changes

--- main.go
+++ main.go
@@ -7,41 +7,54 @@ import (
   "io/ioutil"
 )
 
-func myfunc()[]*http.Response {
+type Answer struct {
+  id		string
+  result    []byte
+  err      	error
+}
+
+func myfunc() []*Answer{
 	var ab = []string{"a","b","c","d","e",}
 	key := ""
 	addr := "https://www.googleapis.com/urlshortener/v1/url?shortUrl=http://goo.gl/"
-   
-    ch := make(chan *http.Response)
-    responses := []*http.Response{}
+	
+    ch := make(chan *Answer)
+    results := []*Answer{}
 	for _, a := range ab{
 		for _, b := range ab{
 			for _, c := range ab{
 				for _, d := range ab{
 					id := fmt.Sprintf("%s%s%s%s",a,b,c,d)
 					url := fmt.Sprintf("%s%s&key=%s",addr,id,key)
-					go func (url string){
-						resp, _:= http.Get(url)
-						ch <- resp
-					}(url)	
+					go func (url string, id string){
+						resp, err:= http.Get(url)
+						result, _ := ioutil.ReadAll(resp.Body)
+					    resp.Body.Close()
+						ch <- &Answer{id, result, err}
+					}(url,id)	
 				}
 			}
 		}
 	}
 	for {
 	    r := <-ch
-	    responses = append(responses, r)
-	    if len(responses) == int(math.Pow(5,4)) {
-			return responses
+	    results = append(results, r)
+	    if len(results) == int(math.Pow(5,4)) {
+			close(ch)
+			return results
 		}
 	}
-	return responses
+	return results
 }
 
 func main() {
-	res := myfunc()
-	for _, r := range res{
-		body, _ := ioutil.ReadAll(r.Body)
-		fmt.Printf("%s",body)
+	results := myfunc()
+	for _, r := range results{
+		if r.err != nil{
+			fmt.Println(r.err)
+		}else{
+			fmt.Println(r.id)
+			fmt.Printf("%s\n",r.result)
+		}
 	}
 }
