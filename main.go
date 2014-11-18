41f8cf2e34e49d6dc3cadc1d9e8f49e2d4661cb9
Nick Triantafyllidis <ntrianta@lab.grid.auth.gr>
2014-11-18 14:07:30 +0100

    totally different stuff

--- main.go
+++ main.go
@@ -8,61 +8,62 @@ import (
 	//"sync"
 )
 
-type Answer struct {
-	short string
-	long  string
-	err   error
-}
+// type Answer struct {
+// 	short string
+// 	long  string
+// 	err   error
+// }
 
 type Long struct {
 	LongUrl string `json:longUrl`
 }
 
-func myfunc() []*Answer {
+func myFunc() {
 	var ab = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
-	//var wg sync.WaitGroup
 	key := ""
 	addr := "https://www.googleapis.com/urlshortener/v1/url?shortUrl=http://goo.gl/"
 
-	results := []*Answer{}
+//	results := []*Answer{}
 
 	for _, c := range ab {
-		ch := make(chan *Answer)
+//		ch := make(chan *Answer)
 		for _, d := range ab {
 			id := fmt.Sprintf("aa%s%s", c, d)
 			url := fmt.Sprintf("%s%s&key=%s", addr, id, key)
-			go func(url string, id string) {
+			go func(url string) {
 				l := Long{}
-				resp, err := http.Get(url)
+				resp, _ := http.Get(url)
 				result, _ := ioutil.ReadAll(resp.Body)
 				resp.Body.Close()
 				e := json.Unmarshal(result, &l)
 				if e != nil {
-					ch <- &Answer{"problem", "problem", e}
+					fmt.Println("Problem")
 				} else {
-					ch <- &Answer{id, l.LongUrl, err}
+					fmt.Println(l.LongUrl)
+					//ch <- &Answer{id, l.LongUrl, err}
 				}
-			}(url, id)
+			}(url)
 		}
-		for {
-			r := <-ch
-			results = append(results, r)
-			if len(results) == 62 {
-				close(ch)
-				break
-			}
-		}
+		// for {
+// 			r := <-ch
+// 			results = append(results, r)
+// 			if len(results) == 62 {
+// 				close(ch)
+// 				break
+// 			}
+// 		}
 	}
-	return results
+	//return results
 }
 
 func main() {
-	results := myfunc()
-	for _, r := range results {
-		if r.err != nil {
-			fmt.Println(r.err)
-		} else {
-			fmt.Println(r.long)
-		}
-	}
+	myFunc()
+	// results := myfunc()
+// 	for _, r := range results {
+// 		if r.err != nil {
+// 			fmt.Println(r.err)
+// 		} else {
+// 			fmt.Println(r.long)
+// 		}
+// 	}
 }
