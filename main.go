e1d6bbc0436670bcdeb51dc8d814848d6ac8cabe
Nick Triantafyllidis <ntrianta@lab.grid.auth.gr>
2014-11-18 23:06:09 +0100

    refactoring

--- main.go
+++ main.go
@@ -5,28 +5,20 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	//"sync"
+	"sync"
 )
-
-// type Answer struct {
-// 	short string
-// 	long  string
-// 	err   error
-// }
 
 type Long struct {
 	LongUrl string `json:longUrl`
 }
 
-func myFunc() {
-	var ab = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
-	key := ""
+func crawl(half []string,ab []string) {
+
+	var wg sync.WaitGroup
+	key := "AIzaSyBddcLjJmXVdGEHxkr9QCIgG63vsxM9fjQ"
 	addr := "https://www.googleapis.com/urlshortener/v1/url?shortUrl=http://goo.gl/"
-
-//	results := []*Answer{}
-
-	for _, c := range ab {
-//		ch := make(chan *Answer)
+	wg.Add(1922)
+	for _, c := range half {
 		for _, d := range ab {
 			id := fmt.Sprintf("aa%s%s", c, d)
 			url := fmt.Sprintf("%s%s&key=%s", addr, id, key)
@@ -36,34 +28,22 @@ func myFunc() {
 				result, _ := ioutil.ReadAll(resp.Body)
 				resp.Body.Close()
 				e := json.Unmarshal(result, &l)
+				wg.Done()
 				if e != nil {
 					fmt.Println("Problem")
 				} else {
-					fmt.Println(l.LongUrl)
-					//ch <- &Answer{id, l.LongUrl, err}
+					fmt.Printf("%s\n", l.LongUrl)
 				}
 			}(url)
 		}
-		// for {
-// 			r := <-ch
-// 			results = append(results, r)
-// 			if len(results) == 62 {
-// 				close(ch)
-// 				break
-// 			}
-// 		}
 	}
-	//return results
+	wg.Wait()
 }
 
 func main() {
-	myFunc()
-	// results := myfunc()
-// 	for _, r := range results {
-// 		if r.err != nil {
-// 			fmt.Println(r.err)
-// 		} else {
-// 			fmt.Println(r.long)
-// 		}
-// 	}
+	var ab = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+	var half1 = []string{"0", "1", "2", "3", "4", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"}
+	var half2 = []string{"5", "6", "7", "8", "9", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+	crawl(half1,ab)
+	crawl(half2,ab)
 }
