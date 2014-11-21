9d90d49b1dde48a75058e7451c7721e1a05f9f88
Nick Triantafyllidis <ntrianta@lab.grid.auth.gr>
2014-11-21 14:47:27 +0100

    added positional arguments support

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"sync"
 	"time"
@@ -13,15 +15,14 @@ type Long struct {
 	LongUrl string `json:longUrl`
 }
 
-func crawl(half []string, ab []string) {
+func crawl(half []string, ab []string, a string, key string) {
 	var wg sync.WaitGroup
-	key := ""
 	addr := "https://www.googleapis.com/urlshortener/v1/url?shortUrl=http://goo.gl/"
 	for _, b := range ab {
 		wg.Add(1922)
 		for _, c := range half {
 			for _, d := range ab {
-				id := fmt.Sprintf("0%s%s%s", b, c, d)
+				id := fmt.Sprintf("%s%s%s%s", a, b, c, d)
 				url := fmt.Sprintf("%s%s&key=%s", addr, id, key)
 				go func(url string) {
 					l := Long{}
@@ -47,11 +48,24 @@ func crawl(half []string, ab []string) {
 }
 
 func main() {
+
 	var ab = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 	var half1 = []string{"0", "1", "2", "3", "4", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"}
 	var half2 = []string{"5", "6", "7", "8", "9", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
-	crawl(half1, ab)
-	fmt.Println("RETURNED TO MAIN")
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) < 2 {
+		log.Fatal("This program will expand 238,328 urls from goo.gl.\nUsage: googlcrawler <startingString> <APIKey>")
+	}
+
+	start := args[0]
+	if len(start) > 3 {
+		log.Fatal("Starting string must be up to 3 characters long")
+	}
+
+	key := args[1]
+	crawl(half1, ab, start, key)
 	time.Sleep(10 * time.Second)
-	crawl(half2, ab)
+	crawl(half2, ab, start, key)
 }
